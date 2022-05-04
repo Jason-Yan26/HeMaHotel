cd7bef37caeb88e1cddb72d4857c8df37e722854
yzh <459155106@qq.com>
2022-05-04 10:05:29 +0800

    Initial commit

---
+++ src/main/java/com/example/hemahotel/HeMaHotelApplication.java
@@ -0,0 +1,14 @@
+package com.example.hemahotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeMaHotelApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HeMaHotelApplication.class, args);
+    }
+
+}
