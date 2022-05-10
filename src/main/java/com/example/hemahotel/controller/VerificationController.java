80311fdc4fb51689777fdf60c49625c5dd3d5a8e
yzh <459155106@qq.com>
2022-05-10 16:59:54 +0800

    增加 验证码获取 接口

---
+++ src/main/java/com/example/hemahotel/controller/VerificationController.java
@@ -0,0 +1,38 @@
+package com.example.hemahotel.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.hemahotel.service.EmailService;
+import com.example.hemahotel.utils.ResponseUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/verification")
+public class VerificationController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/email")
+    public ResponseUtils emailGet(@RequestBody JSONObject jsonObject){
+
+        String receiver = jsonObject.getString("email");
+
+        return emailService.sendVerCode(receiver);
+    }
+
+    @PostMapping("/phone")
+    public ResponseUtils phoneGet(@RequestBody JSONObject jsonObject){
+
+        String receiver = jsonObject.getString("phone");
+
+        //手机验证码服务还未接入,待后期加入
+        JSONObject jsonObject1 = new JSONObject();
+        jsonObject1.put("verCodeId",6);
+        return ResponseUtils.response(200, "验证码发送成功", jsonObject1);
+    }
+
+}
---
+++ src/main/java/com/example/hemahotel/dao/VerificationRepository.java
@@ -0,0 +1,8 @@
+package com.example.hemahotel.dao;
+
+import com.example.hemahotel.entity.Verification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerificationRepository extends JpaRepository<Verification,Long> {
+
+}
