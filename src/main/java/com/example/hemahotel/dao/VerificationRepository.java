cfd534f7d99e4d0ea72b69b6140d28b89ed531f6
zouyingcao <1799771645@qq.com>
2022-05-10 15:37:59 +0800

    verfication

---
+++ src/main/java/com/example/hemahotel/dao/VerificationRepository.java
@@ -0,0 +1,4 @@
+package com.example.hemahotel.dao;
+
+public interface VerificationRepository extends org.springframework.data.jpa.repository.JpaRepository<com.example.hemahotel.entity.Verification, java.lang.Long> {
+}
