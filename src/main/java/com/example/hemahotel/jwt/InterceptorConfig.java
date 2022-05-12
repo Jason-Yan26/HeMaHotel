568ececac8ecea57864b6b0ceab16d749ba2c48d
yzh <459155106@qq.com>
2022-05-12 10:08:29 +0800

    完善 拦截器功能

--- src/main/java/com/example/hemahotel/jwt/InterceptorConfig.java
+++ src/main/java/com/example/hemahotel/jwt/InterceptorConfig.java
@@ -1,14 +1,17 @@
 package com.example.hemahotel.jwt;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new JWTInterceptors())
-                .addPathPatterns("/user/test")  //用于设置拦截器的过滤路径规则
-                .excludePathPatterns("/api/user/login/password")
-                .excludePathPatterns("/api/user/register");//用于设置不需要拦截的过滤规则
+                .addPathPatterns("/api/user")//用于设置拦截器的过滤路径规则
+                .addPathPatterns("/hello")
+                .excludePathPatterns("/api/user/login/password")//用于设置不需要拦截的过滤规则
+                .excludePathPatterns("/api/user/register");
     }
 }
--- src/main/java/com/example/hemahotel/jwt/JWTInterceptors.java
+++ src/main/java/com/example/hemahotel/jwt/JWTInterceptors.java
@@ -24,18 +24,22 @@ public class JWTInterceptors implements HandlerInterceptor {
             return true;  // 放行请求
         } catch (SignatureVerificationException e) {
             e.printStackTrace();
-            map.put("msg","无效签名！");
+            map.put("code",408);
+            map.put("msg","token无效签名！");
         }catch (TokenExpiredException e){
             e.printStackTrace();
+            map.put("code",408);
             map.put("msg","token过期");
         }catch (AlgorithmMismatchException e){
             e.printStackTrace();
-            map.put("msg","算法不一致");
+            map.put("code",408);
+            map.put("msg","token算法不一致");
         }catch (Exception e){
             e.printStackTrace();
+            map.put("code",408);
             map.put("msg","token无效！");
         }
-        map.put("state",false);  // 设置状态
+        map.put("data",null);  // 设置状态
         // 将map以json的形式响应到前台  map --> json  (jackson)
         String json = new ObjectMapper().writeValueAsString(map);
         response.setContentType("application/json;charset=UTF-8");
